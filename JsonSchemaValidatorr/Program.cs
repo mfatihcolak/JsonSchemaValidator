afc9e447ab87674e5277d2d1b20d6182c6c44af5
Muhammed Fatih Çolak <mfatihcolak1@gmail.com>
2022-09-21 16:07:24 +0300

    Add project files.

---
+++ JsonSchemaValidatorr/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+using System.Reflection.PortableExecutable;
+using Newtonsoft.Json.Schema;
+
+//var schema = JSchema.Parse(File.ReadAllText(@"C:\Users\alper\Desktop\BookShop\JsonSchemadotNet\schemaone.json"));
+
+
+JObject patient = JObject.Parse(@"{
+    'id': 1,
+    'name':'fatih',
+    'gender':'male',
+    'phone':'(555)555-5555',
+    'address': {'street': 'kalemkar sokak','city':'Burdur','country': 'turkey','postCode':15100}
+}"); 
+
+using (StreamReader file = File.OpenText(@"C:\Users\pardu\Source\Repos\JsonSchemaValidatorr\JsonSchemaValidatorr\JsonSchema.json"))
+using (JsonTextReader reader = new JsonTextReader(file))
+{
+    JSchemaUrlResolver resolver = new JSchemaUrlResolver();
+
+    JSchema schema2 = JSchema.Load(reader, new JSchemaReaderSettings
+    {
+        Resolver = resolver,
+        // where the schema is being loaded from
+        // referenced 'address.json' schema will be loaded from disk at 'c:\address.json'
+        BaseUri = new Uri(@"C:\Users\pardu\Source\Repos\JsonSchemaValidatorr\JsonSchemaValidatorr\JsonSchema.json")
+    });
+
+    Console.WriteLine(schema2);
+    var valid = patient.IsValid(schema2, out IList<string> messages);
+    Console.WriteLine(valid);
+    // validate JSON
+    foreach (var message in messages)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+
+//var valid = patient.IsValid(schema2, out IList<string> messages);
+//Console.WriteLine(valid);
